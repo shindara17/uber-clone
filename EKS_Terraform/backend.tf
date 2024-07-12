3e68231ff7932286f96f6210d7804fe5194bcb5c
shindara17 <167001355+shindara17@users.noreply.github.com>
2024-07-12 06:32:07 -0700

    Update backend.tf

--- EKS_Terraform/backend.tf
+++ EKS_Terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "shindara-docker-tf" # Replace with your actual S3 bucket name
+    bucket = "shindara-docker-tfstate" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "us-east-1"
   }
