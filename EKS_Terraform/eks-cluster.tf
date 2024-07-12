e7fa4866281b72a16a24285fe9d926e8b08be864
shindara17 <167001355+shindara17@users.noreply.github.com>
2024-07-12 13:59:54 -0700

    Update eks-cluster.tf

--- EKS_Terraform/eks-cluster.tf
+++ EKS_Terraform/eks-cluster.tf
@@ -45,7 +45,7 @@ resource "aws_eks_cluster" "example" {
 }
 
 resource "aws_iam_role" "example1" {
-  name = "eks-node-group-cloud"
+  name = "eks-node-group-cloud-new"
 
   assume_role_policy = jsonencode({
     Statement = [{
