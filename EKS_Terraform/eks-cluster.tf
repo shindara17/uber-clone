4778e66594f04d350547f174aa1da007949ad7c3
shindara17 <167001355+shindara17@users.noreply.github.com>
2024-07-12 05:54:16 -0700

    Update eks-cluster.tf

--- EKS_Terraform/eks-cluster.tf
+++ EKS_Terraform/eks-cluster.tf
@@ -29,7 +29,7 @@ data "aws_vpc" "default" {
 }
 #cluster provision
 resource "aws_eks_cluster" "example" {
-  name     = "EKS_CLOUD"
+  name     = "EKS_DEMO"
   role_arn = aws_iam_role.example.arn
 
 
